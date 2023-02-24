8250ccbe2e080ed29af94dcf0976833b83d393dc
Berk <berrkkaya@gmail.com>
2023-02-24 14:27:38 +0300

    Update Program.cs

--- GithubDeneme/Program.cs
+++ GithubDeneme/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine(  "Ceren");
+Console.WriteLine("Ceren");
 Console.WriteLine("Enes Buğra Cengiz");
 Console.WriteLine("Zeynep");
+Console.WriteLine("Berk Kaya");
