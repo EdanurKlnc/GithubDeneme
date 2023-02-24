355e324c524f85aa5325ae1e170c41c12676150e
SABAH <SABAH@DESKTOP-E30TBPJ>
2023-02-24 14:20:10 +0300

    Edanur

--- GithubDeneme/Program.cs
+++ GithubDeneme/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine(  "Ceren");
+Console.WriteLine("Ceren");
+Console.WriteLine("Edanur Kılınç");
