8ec38c289b688edc479ce2a30fc388120c1b404d
Gamze Turap <SABAH@DESKTOP-E30TBPJ>
2023-02-24 14:18:19 +0300

    ...

--- GithubDeneme/Program.cs
+++ GithubDeneme/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello,hsjdklfşiöljkhg World!");
 Console.WriteLine(  "Ceren");
+Console.WriteLine(  "Gamze Turap");
